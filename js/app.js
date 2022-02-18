0c012c41625d77018b063ea1b807a88c688dd328
Rakesh Rajeev <palanrakesh@gmail.com>
2022-02-18 08:55:11 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+$('.slider').slick({
+ arrows:false,
+ dots:true,
+ appendDots:'.slider-dots',
+ dotsClass:'dots',
+ autoplay: true,
+ autoplaySpeed: 1000,
+ infinite: false,
+ speed: 100,
+ fade: false,
+ slide: 'div',
+ cssEase: 'linear',
+ onAfterChange: function(){
+     //check the length of total items in .slide container
+     //if that number is the same with the number of the last slide
+     //Then pause the slider
+     if( item_length == slider.slickCurrentSlide() ){
+         //this should do the same thing -> slider.slickPause();
+         slider.slickSetOption("autoplay",false,false)
+     };
+ }
+});
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let shortCut=document.querySelector('.shortcut')
+
+
+hamberger.addEventListener('click',function(){
+  mobileNav.classList.add('open');
+})
+
+times.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+})
+
+shortCut.addEventListener('click',function(){
+    times.classList.remove('open')
+})
+
+
+
+});
+
